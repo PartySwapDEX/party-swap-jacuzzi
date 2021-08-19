448fa6b05d79e1f2ada620ad1543639b7eb02d9f
Pedro Medina <pedromedina190@gmail.com>
2021-08-19 17:13:53 -0400

    update tests to meet var names on contract

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,5 @@
 const PartyJacuzzi = artifacts.require("PartyJacuzzi");
 
 module.exports = function (deployer) {
-  return deployer.deploy(PartyJacuzzi, '0x15957be9802B50c6D66f58a99A2a3d73F5aaf615');
+  return deployer.deploy(PartyJacuzzi, '0xEbD7fF328bC30087720e427CB8f11E9Bd8aF7d8A');
 };
--- test/PartyJacuzzi.js
+++ test/PartyJacuzzi.js
@@ -1,36 +1,42 @@
 const PartyJacuzzi = artifacts.require("PartyJacuzzi");
 const YAYToken = artifacts.require('YAYToken');
 
+// May want to substitute here this addres with the actual address on your preferred network
+const YAY_ADDRESS = '0xEbD7fF328bC30087720e427CB8f11E9Bd8aF7d8A';
+
+// May want to toggle comment on this part if testing next in fuji network, to avoid 'Unfinalized data query errors'.
+// contract("PartyJacuzzi", async accounts => {
+//   it("Should be an xYAY token named PartyJacuzzi", async () => {
+//     const instance = await PartyJacuzzi.deployed();
+//     const symbol = await instance.symbol.call();
+//     const name = await instance.name.call();
+//     const decimals = await instance.decimals.call();
+//     assert.equal(symbol.valueOf(), 'xYAY');
+//     assert.equal(name.valueOf(), 'PartyJacuzzi');
+//     assert.equal(decimals.valueOf(), 18);
+//   });
+
+//   it("YAY address must match", async () => {
+//     const instance = await PartyJacuzzi.deployed();
+//     const JacuzziYAY = await instance.yay.call();
+//     assert.equal(YAY_ADDRESS, JacuzziYAY);
+//   });
+
+//   it("Initial early withdrawal fee must be 20%", async () => {
+//     const instance = await PartyJacuzzi.deployed();
+//     const earlyWithdrawalFee = await instance.earlyWithdrawalFeePortionFromPercentageBase.call();
+//     const maxFee = await instance.MAX_EARLY_WITHDRAW_FEE_PERCENTAGE_BASE.call();
+//     assert.equal(earlyWithdrawalFee.valueOf(), 20);
+//     assert.equal(maxFee.valueOf(), 100);
+//   });
+// });
+
+// Toggle comment if want to skip this part
 contract("PartyJacuzzi", async accounts => {
-  it("Should be an xYAY token named PartyJacuzzi", async () => {
-    const instance = await PartyJacuzzi.deployed();
-    const symbol = await instance.symbol.call();
-    const name = await instance.name.call();
-    const decimals = await instance.decimals.call();
-    assert.equal(symbol.valueOf(), 'xYAY');
-    assert.equal(name.valueOf(), 'PartyJacuzzi');
-    assert.equal(decimals.valueOf(), 18);
-  });
-
-  it("YAY address must match", async () => {
-    const YAY_ADDRESS = '0xEbD7fF328bC30087720e427CB8f11E9Bd8aF7d8A';
-    const instance = await PartyJacuzzi.deployed();
-    const JacuzziYAY = await instance.yay.call();
-    assert.equal(YAY_ADDRESS, JacuzziYAY);
-  });
-
-  it("Initial early withdrawal fee must be 20%", async () => {
-    const instance = await PartyJacuzzi.deployed();
-    const earlyWithdrawalFee = await instance.earlyWithdrawalFee.call();
-    const maxFee = await instance.MAX_EARLY_WITHDRAW_FEE.call();
-    assert.equal(earlyWithdrawalFee.valueOf(), 2000);
-    assert.equal(maxFee.valueOf(), 10000);
-  });
-
   //costs YAY
   it("Entering with no previous supply should increase balance by 1:1", async () => {
     const instance = await PartyJacuzzi.deployed();
-    const yay = await YAYToken.at("0xEbD7fF328bC30087720e427CB8f11E9Bd8aF7d8A");
+    const yay = await YAYToken.at(YAY_ADDRESS);
     const ammount = web3.utils.toBN(1000);
 
     const initialAccountBalance = await instance.balanceOf(accounts[0]);
@@ -45,7 +51,7 @@ contract("PartyJacuzzi", async accounts => {
   // costs YAY
   it("Entering with previous supply and more jacuzzi YAY balance should increase balance with a ratio difference", async () => {
     const instance = await PartyJacuzzi.deployed();
-    const yay = await YAYToken.at("0xEbD7fF328bC30087720e427CB8f11E9Bd8aF7d8A");
+    const yay = await YAYToken.at(YAY_ADDRESS);
 
     const ammount = web3.utils.toBN(1000);
 
@@ -77,8 +83,8 @@ contract("PartyJacuzzi", async accounts => {
 
   it("Leaving before unlock date, should give us our YAY back minus fees", async () => {
     const instance = await PartyJacuzzi.deployed();
-    const yay = await YAYToken.at("0xEbD7fF328bC30087720e427CB8f11E9Bd8aF7d8A");
-    const ammount = web3.utils.toBN(1000000000000000000);
+    const yay = await YAYToken.at(YAY_ADDRESS);
+    const ammount = web3.utils.toBN(1000);
 
     const initialYAYBalance = await yay.balanceOf(accounts[0]);
     const initialShares = await instance.balanceOf(accounts[0]);
@@ -95,8 +101,8 @@ contract("PartyJacuzzi", async accounts => {
     const sharesToTakeBack = middleShares.gte(initialShares) ? middleShares.sub(initialShares) : initialShares.sub(middleShares);
     const what = sharesToTakeBack.mul(jacuzziYayBalance).div(instanceTotalShares);
 
-    const earlyWithdrawalFee = await instance.earlyWithdrawalFee();
-    const MAX_EARLY_WITHDRAW_FEE = await instance.MAX_EARLY_WITHDRAW_FEE();
+    const earlyWithdrawalFee = await instance.earlyWithdrawalFeePortionFromPercentageBase();
+    const MAX_EARLY_WITHDRAW_FEE = await instance.MAX_EARLY_WITHDRAW_FEE_PERCENTAGE_BASE();
     const toBurn = what.mul(earlyWithdrawalFee).div(MAX_EARLY_WITHDRAW_FEE);
 
     //attepmt to take your YAYs back
@@ -112,4 +118,4 @@ contract("PartyJacuzzi", async accounts => {
     }
 
   });
-});
+})
